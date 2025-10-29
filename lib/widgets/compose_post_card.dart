8ecbd30c5b49204a00f9fa9f880abf474608b9c5
daoporo102 <truongquangdao102@gmail.com>
2025-10-29 17:07:21 +0700

    feat(post): Add Post Card on Home page

---
+++ lib/widgets/compose_post_card.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_app/screens/add_post_screen.dart';
+import 'package:social_media_app/utils/colors.dart';
+
+class ComposePostCard extends StatelessWidget {
+  const ComposePostCard({super.key});
+
+  void _goToCreatePost(BuildContext context) {
+    // Navigate to the post creation screen
+    Navigator.of(
+      context,
+    ).push(MaterialPageRoute(builder: (context) => const AddPostScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      color: onPrimaryColor,
+
+      elevation: 0.5,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 8),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(12, 10, 12, 8),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundImage: AssetImage(
+                    "assets/images/default-avatar.png",
+                  ),
+                ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: TextButton(
+                    style: ButtonStyle(
+                      backgroundColor: WidgetStateProperty.resolveWith((
+                        states,
+                      ) {
+                        if (states.contains(WidgetState.pressed)) {
+                          return textFieldBackgroundColor.withValues(
+                            alpha: 0.85,
+                          );
+                        }
+                        if (states.contains(WidgetState.hovered)) {
+                          return textFieldBackgroundColor.withValues(
+                            alpha: 0.95,
+                          );
+                        }
+                        return textFieldBackgroundColor;
+                      }),
+                      foregroundColor: WidgetStateProperty.all(
+                        primaryTextColor,
+                      ),
+                      overlayColor: WidgetStateProperty.resolveWith((states) {
+                        if (states.contains(WidgetState.hovered) ||
+                            states.contains(WidgetState.pressed)) {
+                          return appPrimaryColor.withValues(alpha: 0.08);
+                        }
+                        return Colors.transparent;
+                      }),
+                      shape: WidgetStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16),
+                        ),
+                      ),
+                      padding: WidgetStateProperty.all(
+                        const EdgeInsets.symmetric(
+                          vertical: 12,
+                          horizontal: 16,
+                        ),
+                      ),
+                    ),
+                    onPressed: () => _goToCreatePost(context),
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.mode_edit_outline_outlined,
+                          color: primaryTextColor,
+                          size: 16,
+                        ),
+                        SizedBox(width: 8),
+                        Text(
+                          "Bạn đang nghĩ gì?",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: primaryTextColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
