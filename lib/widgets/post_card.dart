f429efa4564838c89881212a018d5fa10d1cd101
daoporo102 <truongquangdao102@gmail.com>
2025-11-02 21:05:10 +0700

    Storing comment in Firestore

--- lib/widgets/post_card.dart
+++ lib/widgets/post_card.dart
@@ -181,7 +181,9 @@ class _PostCardState extends State<PostCard> {
               ),
               IconButton(
                 onPressed: () => Navigator.of(context).push(
-                  MaterialPageRoute(builder: (context) => CommentsScreen()),
+                  MaterialPageRoute(
+                    builder: (context) => CommentsScreen(snap: widget.snap),
+                  ),
                 ),
                 icon: const Icon(Icons.comment_outlined),
               ),
