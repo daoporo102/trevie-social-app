d4ee430b56d3dece93540609b87977f8315354d6
daoporo102 <truongquangdao102@gmail.com>
2025-11-14 16:55:12 +0700

    feature(add-post): Update UI on Web & Android

--- lib/utils/global_variables.dart
+++ lib/utils/global_variables.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:social_media_app/screens/add_post_screen.dart';
 import 'package:social_media_app/screens/feed_screen.dart';
 import 'package:social_media_app/screens/more_screen.dart';
 import 'package:social_media_app/screens/profile_screen.dart';
 import 'package:social_media_app/screens/search_screen.dart';
+import 'package:social_media_app/utils/colors.dart';
 
 const webScreenSize = 600;
 
@@ -35,4 +36,37 @@ List<Widget> homeWebScreenItems() {
     ProfileScreen(uid: uid),
     MoreScreen(uid: uid),
   ];
+}
+
+Widget customTextButton({
+  required IconData icon,
+  required String label,
+  required VoidCallback onPressed,
+}) {
+  return TextButton(
+    onHover:(value) => {
+      // You can add hover effects
+      if (value) {
+        // Mouse is hovering
+
+      } else {
+        // Mouse is not hovering
+      }
+    },
+    onPressed: onPressed,
+    child: Row(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Icon(icon, color: primaryTextColor),
+        SizedBox(width: 4),
+        Text(
+          label,
+          style: TextStyle(
+            color: primaryTextColor,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    ),
+  );
 }
