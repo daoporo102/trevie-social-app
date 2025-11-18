2208e6c3228fdc89ea181ea784af91ac0a6ed6ed
daoporo102 <truongquangdao102@gmail.com>
2025-11-18 17:28:08 +0700

    refactor(media-button):Change media button(img,vid,file) from customTextButton into Icon Button

--- lib/screens/add_post_screen.dart
+++ lib/screens/add_post_screen.dart
@@ -361,19 +361,62 @@ class _AddPostScreenState extends State<AddPostScreen> {
                   ? Row(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
                       children: [
-                        customTextButton(
-                          icon: Icons.photo_library,
-                          label: 'Ảnh',
+                        IconButton(
+                          icon: Row(
+                            children: const [
+                              Icon(
+                                Icons.image_outlined,
+                                color: primaryTextColor,
+                              ),
+                              SizedBox(width: 4),
+                              Text(
+                                'Ảnh',
+                                style: TextStyle(
+                                  color: primaryTextColor,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
                           onPressed: () => _selectImage(context),
                         ),
-                        customTextButton(
-                          icon: Icons.videocam,
-                          label: 'Video',
+                        IconButton(
+                          icon: Row(
+                            children: const [
+                              Icon(
+                                Icons.video_camera_back_outlined,
+                                color: primaryTextColor,
+                              ),
+                              SizedBox(width: 4),
+                              Text(
+                                'Video',
+                                style: TextStyle(
+                                  color: primaryTextColor,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
                           onPressed: () {},
                         ),
-                        customTextButton(
-                          icon: Icons.file_copy,
-                          label: 'Tài liệu',
+                        IconButton(
+                          icon: Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: const [
+                              Icon(
+                                Icons.file_open_outlined,
+                                color: primaryTextColor,
+                              ),
+                              SizedBox(width: 4),
+                              Text(
+                                'Tài liệu',
+                                style: TextStyle(
+                                  color: primaryTextColor,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
                           onPressed: () {},
                         ),
                       ],
