bef2559a728209781954ea690f2969abe45f310c
daoporo102 <truongquangdao102@gmail.com>
2025-12-05 09:18:42 +0700

    feat(update-users): Add createdAt property to Auth Methods

--- lib/resources/auth_methods.dart
+++ lib/resources/auth_methods.dart
@@ -22,6 +22,7 @@ class AuthMethods {
         following: [],
         bio: '',
         dateOfBirth: DateTime.now(),
+        createdAt: DateTime.now(),
       );
     }
 
@@ -39,6 +40,7 @@ class AuthMethods {
         following: [],
         bio: '',
         dateOfBirth: DateTime.now(),
+        createdAt: DateTime.now(),
       );
     }
     return model.User.fromSnap(snap);
@@ -79,6 +81,7 @@ class AuthMethods {
           following: [],
           bio: '',
           dateOfBirth: DateTime.now(),
+          createdAt: DateTime.now(),
         );
 
         //adding user in our database
